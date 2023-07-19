e1961e873677ba320fd10d751cbb0e7df64975cf
RyuMinSu <sonicsu94@gmail.com>
2023-07-19 23:26:40 +0900

    accountapp createview

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,8 +1,9 @@
-from accountapp.views import home
+from accountapp.views import home, AccountCreateView
 from django.urls import path
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('home/', home, name='home')
+    path('home/', home, name='home'),
+    path('create/', AccountCreateView.as_view(), name="create")
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,25 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+
+
 
 # Create your views here.
 def home(request):
     return render(request, 'accountapp/home.html')
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'accountapp/create.html'
+    success_url = reverse_lazy('accountapp:home')
+
+
+
+
+
+
