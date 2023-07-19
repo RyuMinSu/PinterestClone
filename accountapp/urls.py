c8e6329977897960e5bd5033c31aede9c0aad0f5
RyuMinSu <sonicsu94@gmail.com>
2023-07-20 00:02:22 +0900

    accountapp login/logout

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.views import LoginView, LogoutView
+
 from accountapp.views import home, AccountCreateView
 from django.urls import path
 
@@ -5,5 +7,7 @@ app_name = 'accountapp'
 
 urlpatterns = [
     path('home/', home, name='home'),
-    path('create/', AccountCreateView.as_view(), name="create")
+    path('create/', AccountCreateView.as_view(), name="create"),
+    path('login/', LoginView.as_view(template_name="accountapp/login.html"), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
 ]
