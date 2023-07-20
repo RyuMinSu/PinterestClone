ab8c9df99d909f648767c6449a3ac7485b396b58
RyuMinSu <sonicsu94@gmail.com>
2023-07-20 11:05:04 +0900

    accountapp updateview

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,11 +1,10 @@
+from accountapp.forms import AccountUpdateForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
-
-
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, DetailView, UpdateView
 
 
 # Create your views here.
@@ -17,6 +16,21 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     template_name = 'accountapp/create.html'
     success_url = reverse_lazy('accountapp:home')
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    context_object_name = 'target_user'
+    template_name = 'accountapp/update.html'
+    success_url = reverse_lazy('accountapp:home')
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 
 
