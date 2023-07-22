c647af6f8f6a66c0d701834dfa64eb65af8b48b9
RyuMinSu <sonicsu94@gmail.com>
2023-07-22 22:19:02 +0900

    articleapp listview complete

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from articleapp.views import ArticleCreateView, ArticleListView
+from articleapp.views import ArticleCreateView, ArticleListView, ArticleDetailView
 
 app_name = "articleapp"
 
 urlpatterns = [
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('list/', ArticleListView.as_view(), name='list'),
+    path('detail/<int:pk>', ArticleDetailView.as_view(), name="detail")
 ]
--- articleapp/views.py
+++ articleapp/views.py
@@ -1,16 +1,21 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView
+from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, ListView, DetailView
 
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
 
 
 # Create your views here.
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ArticleCreateView(CreateView):
     model = Article
     form_class = ArticleCreationForm
     template_name = 'articleapp/create.html'
-    # success_url =
+    success_url = reverse_lazy('articleapp:list')
 
     def form_valid(self, form):
         temp_article = form.save(commit=False)
@@ -20,7 +25,10 @@ class ArticleCreateView(CreateView):
 
 class ArticleListView(ListView):
     model = Article
-    # context_object_name = 'article_list'
+    context_object_name = 'article_list'
     template_name = 'articleapp/list.html'
 
-
+class ArticleDetailView(DetailView):
+    model = Article
+    context_object_name = 'target_article'
+    template_name = "articleapp/detail.html"
