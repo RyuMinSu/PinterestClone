f46d91679debf4a6c97e1150f85870cd5a309aef
RyuMinSu <sonicsu94@gmail.com>
2023-07-22 12:41:20 +0900

    profileapp createview

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import CreateView
 
+from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
 
 # Create your views here.
 class ProfileCreateView(CreateView):
     model = Profile
+    form_class = ProfileCreationForm #user 제외한 form
+    template_name = 'profileapp/create.html'
+    
+    def form_valid(self, form):
+        temp_profile = form.save(commit=False)
+        temp_profile.user = self.request.user
+        temp_profile.save()
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+    
+    # 로그인 한사람만 프로필을 만들 수 있게
+    # 나만 내 프로필을 만들 수 있게
