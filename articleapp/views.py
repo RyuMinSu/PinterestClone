513aa8b3d7b1ec81367ff50743334c9a19e5c109
RyuMinSu <sonicsu94@gmail.com>
2023-07-22 20:43:27 +0900

    articleapp createview

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
 
+from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
 
 
 # Create your views here.
 class ArticleCreateView(CreateView):
     model = Article
+    form_class = ArticleCreationForm
+    template_name = 'articleapp/create.html'
+    # success_url =
+
+    def form_valid(self, form):
+        temp_article = form.save(commit=False)
+        temp_article.writer = self.request.user
+        temp_article.save()
+        return super().form_valid(form)
+
