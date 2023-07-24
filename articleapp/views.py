c3056df3dd286e937e9efc77848a27aec66c6212
RyuMinSu <sonicsu94@gmail.com>
2023-07-24 09:37:41 +0900

    pagination

--- articleapp/views.py
+++ articleapp/views.py
@@ -29,6 +29,7 @@ class ArticleListView(ListView):
     model = Article
     context_object_name = 'article_list'
     template_name = 'articleapp/list.html'
+    paginate_by = 6
 
 class ArticleDetailView(DetailView):
     model = Article
