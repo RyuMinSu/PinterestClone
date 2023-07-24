11ce534717c8083dbf72b604828842aeb3a3a12b
RyuMinSu <sonicsu94@gmail.com>
2023-07-24 13:40:06 +0900

    projectapp create and detailview

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.urls import reverse
+from django.views.generic import CreateView, DetailView, ListView
 
+from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
 
 # Create your views here.
 class ProjectCreateView(CreateView):
     model = Project
+    form_class = ProjectCreationForm
+    template_name = 'projectapp/create.html'
+    
+    def form_valid(self, form):
+        temp_project = form.save(commit=True)
+        temp_project.writer = self.request.user
+        temp_project.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('projectapp:detail', kwargs={'pk':self.object.pk})
+
+class ProjectDetailView(DetailView):
+    model = Project
+    context_object_name = 'target_project'
+    template_name = 'projectapp/detail.html'
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = "projectapp/list.html"
